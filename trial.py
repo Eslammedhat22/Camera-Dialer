aa9a4cda090db818bb17808ad6231f4484b67760
Eslammedhat22 <33896395+Eslammedhat22@users.noreply.github.com>
2019-04-28 17:15:18 +0200

    Update trial.py

--- trial.py
+++ trial.py
@@ -2,7 +2,11 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 #code which helps initialize our server
-app = Flask(__name__) 
+app = Flask(__name__)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    
 @app.route('/', methods=['POST'])
 def result():
    print("enered")
